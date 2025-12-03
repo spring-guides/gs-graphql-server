36a6da7e6583bedf071b80b06a00f5a2252140c4
Brian Clozel <brian.clozel@broadcom.com>
2025-12-03 11:12:33 +0100

    Upgrade to Spring Boot 4.0.0

--- complete/src/test/java/com/example/graphqlserver/BookControllerTests.java
+++ complete/src/test/java/com/example/graphqlserver/BookControllerTests.java
@@ -2,7 +2,7 @@ package com.example.graphqlserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.boot.graphql.test.autoconfigure.GraphQlTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
 @GraphQlTest(BookController.class)
